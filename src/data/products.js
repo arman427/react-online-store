6d190e9e1ba90296434de6ec57b8e5f445147cea
Arman <babayananuta11@gmail.com>
2025-12-04 22:47:37 +0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,86 @@
+export const products = [
+   {
+      id: 1,
+      name: 'Ноутбук HUAWEI MateBook D16',
+      price: 40999,
+      imageUrl: './../images/nqqo5xd97cac3et_aa5d0d8e.jpg.webp',
+      category: 'Ноутбуки'
+   },
+   {
+      id: 2,
+      name: 'Ноутбук Apple MacBook Air 13 Late',
+      price: 67999,
+      imageUrl: './../images/Без названия.jpg',
+      category: 'Ноутбуки'
+   },
+   {
+      id: 3,
+      name: 'Ноутбук Apple MacBook Air 13 M1',
+      price: 67999,
+      imageUrl: './../images/m81v5q95st0t402nrsirtfta207sta2n.jpg',
+      category: 'Ноутбуки'
+   },
+   {
+      id: 4,
+      name: 'Ноутбук HUAWEI MateBook D14',
+      price: 50999,
+      imageUrl: './../images/13.webp',
+      category: 'Ноутбуки'
+   },
+   {
+      id: 5,
+      name: 'Ноутбук HONOR MagicBook Pro 16 Ultra',
+      price: 115999,
+      imageUrl: './../images/400324377bb.avif',
+      category: 'Ноутбуки'
+   },
+   {
+      id: 6,
+      name: 'Samsung S24 Ultra',
+      price: 76299,
+      imageUrl: './../images/s24.jpg',
+      category: 'Телефоны'
+   },
+   {
+      id: 7,
+      name: 'Samsung S25 Ultra',
+      price: 125990,
+      imageUrl: './../images/s25.jpg',
+      category: 'Телефоны'
+   },
+   {
+      id: 8,
+      name: 'Vivo X200 Ultra 256 ГБ',
+      price: 149999,
+      imageUrl: './../images/vivo200.jpg',
+      category: 'Телефоны'
+   },
+   {
+      id: 9,
+      name: 'Xiaomi 15 Ultra 512 ГБ',
+      price: 104999,
+      imageUrl: './../images/x15u.avif',
+      category: 'Телефоны'
+   },
+   {
+      id: 10,
+      name: 'Xiaomi 15 Ultra 512 ГБ',
+      price: 104999,
+      imageUrl: './../images/x15u.avif',
+      category: 'Телефоны'
+   },
+   {
+      id: 11,
+      name: 'iPhone 16 Pro Max 256 ГБ',
+      price: 131499,
+      imageUrl: './../images/iphone16pm.jpg.webp',
+      category: 'Телефоны'
+   },
+   {
+      id: 12,
+      name: 'iPhone 17 Pro Max 256 ГБ',
+      price: 155999,
+      imageUrl: './../images/iphone17pm.jpg.webp',
+      category: 'Телефоны'
+   },
+]
